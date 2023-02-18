ece5340c20b06bd312c55f406dee4d9793cdb974
unknown <kundanjha826@gmail.com>
2023-02-18 16:43:01 +0530

    added exception handling

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -21,15 +21,15 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
-    public Blog createAndReturnBlog(Integer userId, String title, String content) {
+    public Blog createAndReturnBlog(Integer userId, String title, String content) throws Exception {
         //create a blog at the current time
 
         User user = userRepository1.findById(userId).get();
+        if (user == null) throw new Exception();
 
         Blog blog = new Blog();
         blog.setTitle(title);
         blog.setContent(content);
-        if(user == null) return blog;
         blog.setUser(user);
 
         List<Blog> userBlogs = user.getBlogList();
